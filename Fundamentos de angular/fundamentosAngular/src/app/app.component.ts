36e0496e42eb352cdbe907f37cb6d54186751fd3
Carlos-nicolas <carlosalexis52@hotmail.com>
2022-05-18 16:46:35 -0500

    Event Binding

--- Fundamentos de angular/fundamentosAngular/src/app/app.component.ts
+++ Fundamentos de angular/fundamentosAngular/src/app/app.component.ts
@@ -7,13 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fundamentosAngular';
-    name = 'Nicolas';
-    age = 18;
-    img = 'https://www.w3schools.com/howto/img_avatar.png';
-    btnDisabled = true;
-    person = {
-      name: 'Nicolas',
-      age: 18,
-      avatar: 'https://www.w3schools.com/howto/img_avatar.png'
-
-}}
+  name = 'Nicolas';
+  age = 18;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled = true;
+  person = {
+    name: 'Nicolas',
+    age: 18,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  }
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  };
+  increaseAge(){
+    this.person.age++
+  }
+  reduceAge(){
+    this.person.age--
+  }
+}
